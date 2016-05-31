6182bc8e729a11744e542ee89a15361bfc074475
rpi <rpi>
2016-05-31 11:52:33 +0000

    Improved quality slightly of picamera capture

--- capture/cap_picam.py
+++ capture/cap_picam.py
@@ -8,9 +8,9 @@ import picamera
 
 w_res = 320
 h_res = 240
-fps = 10
+fps = 30
 
-print picamera
+frame_divider = 0
 
 try:
 
@@ -23,14 +23,19 @@ try:
         camera.resolution = (w_res, h_res)
         camera.framerate = fps
 
+        print "Started with framerate: " + str(camera._get_framerate())
+        print "Started with resolution: " + str(camera._get_resolution())
+
         time.sleep(2)
 
         stream = io.BytesIO()
 
         for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-            sock.send(stream.getvalue())
+            if (frame_divider == 0):
+                sock.send(stream.getvalue())
             stream.seek(0)
             stream.truncate()
+            frame_divider = (frame_divider+1)%3
 
 except Exception as e:
     print "Exception: " + str(e)
